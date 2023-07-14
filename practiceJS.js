e66031ced8824bd854dbf2b9b4fd80105315e76c
Jessie He <104229166+jessieh9@users.noreply.github.com>
2023-07-13 22:12:09 -0700

    Update practiceJS.js

--- practiceJS.js
+++ practiceJS.js
@@ -72,3 +72,19 @@ if (guests.includes(name)){
   alert("nah, get outta here.");
 }
 
+// print 1-100, multiple of 3 - fizz, 5 - buzz, both- FizzBuzz
+
+for (let i = 1; i < 101; i++){
+  if ((i % 3 === 0) && (i%5===0)){
+    console.log("FizzBuzz");
+  }
+  else if (i % 3 === 0){
+    console.log("Fizz");
+  }
+  else if (i%5===0){
+    console.log("Buzz");
+  } else{
+    console.log(i);
+  }
+  
+}
