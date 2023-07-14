8094707ff49c56073e2d3e9b6cbee41b13a08378
Jessie He <104229166+jessieh9@users.noreply.github.com>
2023-07-13 22:04:40 -0700

    Update practiceJS.js

--- practiceJS.js
+++ practiceJS.js
@@ -1,3 +1,4 @@
+// strings and manipulation of them
 // var message = prompt("Enter your message.");
 // var used = message.length;
 
@@ -12,45 +13,62 @@
 // newname = firstLetter + restOfName;
 // alert("Hello " + newname + ", Welcome to the course.");
 
+
+// practicing multiple operators and order of operations
 // var dogAge = prompt("Enter your dog's age.");
 // var dogHumanAge = (dogAge - 2) * 4 + 21;
 // alert("Your " + dogAge + " year old dog is "+ dogHumanAge +" years old in human age.");
 
-function getMilk(money) {
-    var costOneBottle = 1.50;
-    var buyBottles = parseInt(money / 1.50);
-    console.log("leaveHouse");
-    console.log("moveRight");
-    console.log("moveRight");
-    console.log("moveUp");
-    console.log("moveUp");
-    console.log("moveUp");
-    console.log("moveUp");
-    console.log("moveRight");
-    console.log("moveRight");
-    console.log("buy " + buyBottles + " bottles of milk.");
-    console.log("moveLeft");
-    console.log("moveLeft");
-    console.log("moveDown");
-    console.log("moveDown");
-    console.log("moveDown");
-    console.log("moveDown");
-    console.log("moveLeft");
-    console.log("moveLeft");
-    console.log("enterHouse");
-}
 
-getMilk(5.50);
+// practicing functions -- with parameters
+// function getMilk(money) {
+//     var costOneBottle = 1.50;
+//     var buyBottles = parseInt(money / 1.50);
+//     console.log("leaveHouse");
+//     console.log("moveRight");
+//     console.log("moveRight");
+//     console.log("moveUp");
+//     console.log("moveUp");
+//     console.log("moveUp");
+//     console.log("moveUp");
+//     console.log("moveRight");
+//     console.log("moveRight");
+//     console.log("buy " + buyBottles + " bottles of milk.");
+//     console.log("moveLeft");
+//     console.log("moveLeft");
+//     console.log("moveDown");
+//     console.log("moveDown");
+//     console.log("moveDown");
+//     console.log("moveDown");
+//     console.log("moveLeft");
+//     console.log("moveLeft");
+//     console.log("enterHouse");
+// }
 
-prompt("What is your name?");
-prompt("What is your crush's name?");
+// getMilk(5.50);
 
-var loveScore = Math.random() * 100;
-loveScore = Math.floor(loveScore) + 1;
 
-if (loveScore > 70){
-  alert("Your love score is " + loveScore + "%. You love each other like Kanye loves Kanye.");
-} else{
-  alert("Your love score is " + loveScore + "%.");
+// practicing random nums
+// prompt("What is your name?");
+// prompt("What is your crush's name?");
+
+// var loveScore = Math.random() * 100;
+// loveScore = Math.floor(loveScore) + 1;
+
+// if (loveScore > 70){
+//   alert("Your love score is " + loveScore + "%. You love each other like Kanye loves Kanye.");
+// } else{
+//   alert("Your love score is " + loveScore + "%.");
+// }
+
+
+// simple program for practicing arrays
+var name = prompt("Enter your name.");
+var guests = ["john", "mary", "dawn"];
+
+if (guests.includes(name)){
+  alert("Welcome " + name + "!");
+} else {
+  alert("nah, get outta here.");
 }
 
