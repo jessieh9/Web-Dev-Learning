83010ceade53ac46a399898b6c72d1b0ba0f05e5
Jessie He <104229166+jessieh9@users.noreply.github.com>
2023-07-18 21:32:05 -0700

    Add files via upload

---
+++ 2.3 NPM/index.js
@@ -0,0 +1,12 @@
+// import generateName from "sillyname";
+
+// var generateName = require('sillyname');  // require the package sillyname
+// var sillyName = generateName();
+
+// console.log(`My name is ${sillyName}.`);
+
+import superheroes from "superheroes";
+// superheroes.all;
+//=> ['3-D Man', 'A-Bomb', …]
+
+console.log(`I am ${superheroes.random()}!`);
