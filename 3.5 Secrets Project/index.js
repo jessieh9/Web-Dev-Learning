4880c5126bc6e67233d2fde2337b9ccd2c8f0a40
Jessie He <104229166+jessieh9@users.noreply.github.com>
2023-07-19 20:03:59 -0700

    Allow index.js to have html with styles

--- 3.5 Secrets Project/index.js
+++ 3.5 Secrets Project/index.js
@@ -4,15 +4,22 @@ import express from "express";
 import bodyParser from "body-parser";
 
 import { dirname } from "path";
+import path from "path";
 import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
 
+app.use(express.static('public'));
+app.use('/html', (req, res, next) => {
+    res.sendFile(path.join(__dirname, 'HTML', 'index.html'));
+});
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
+
     res.sendFile(__dirname + "/public/index.html");
 });
 
