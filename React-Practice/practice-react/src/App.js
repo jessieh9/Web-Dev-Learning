5b13539270fa97d33579c9becad1584b53803949
Jessie He <104229166+jessieh9@users.noreply.github.com>
2023-08-04 11:30:27 -0700

    learning import and export es6

--- React-Practice/practice-react/src/App.js
+++ React-Practice/practice-react/src/App.js
@@ -2,12 +2,21 @@ import React from "react";
 import Heading from "./Header";
 import List from "./List";
 import './App.css';
+import { add, subtract, multiply, divide } from "./calculator"
+
+// 3, 6, 5, 2.5
 
 function App() {
   return (
     <>
       <Heading />,
       <List />,
+      <ul>
+        <li>{add(2, 1)}</li>
+        <li>{subtract(8, 2)}</li>
+        <li>{multiply(1, 5)}</li>
+        <li>{divide(5, 2)}</li>
+      </ul>
     </>
   );
 }
