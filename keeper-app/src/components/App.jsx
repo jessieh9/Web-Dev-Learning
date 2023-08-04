83c593503a5473594585410ce743ab4fbdf52301
Jessie He <104229166+jessieh9@users.noreply.github.com>
2023-08-04 15:30:31 -0700

    Keeper app pt1- working with components

---
+++ keeper-app/src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./Header.jsx";
+import Footer from "./Footer.jsx";
+import Note from "./Note.jsx";
+
+
+function App() {
+    return (
+        <>
+            <Header />
+            <Note />
+            <Footer />
+        </>
+    );
+}
+
+export default App;
