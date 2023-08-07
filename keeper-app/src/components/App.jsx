6090606186a9be9e0adaec0d307dbc9580aaabe4
Jessie He <104229166+jessieh9@users.noreply.github.com>
2023-08-07 15:44:20 -0700

    updated src with notes functionality

--- keeper-app/src/components/App.jsx
+++ keeper-app/src/components/App.jsx
@@ -2,13 +2,22 @@ import React from "react";
 import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
 import Note from "./Note.jsx";
+import notes from "../notes.js";
+
+function createNote(note) {
+    return <Note
+        key={note.key}
+        title={note.title}
+        content={note.content}
+    />;
+}
 
 
 function App() {
     return (
         <>
             <Header />
-            <Note />
+            {notes.map(createNote)}
             <Footer />
         </>
     );
