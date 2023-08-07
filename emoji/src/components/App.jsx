9a59f3547249478c33c61d2044745f7c7414f26e
Jessie He <104229166+jessieh9@users.noreply.github.com>
2023-08-07 09:09:20 -0700

    map for filtering

--- emoji/src/components/App.jsx
+++ emoji/src/components/App.jsx
@@ -2,6 +2,14 @@ import React from "react";
 import Entry from "./Entry";
 import emojipedia from "../emojipedia";
 
+
+// using map for filtering purpose
+// var description = emojipedia.map(function (emoji) {
+//   return emoji.meaning.substring(0, 100);
+// });
+
+// console.log(description);
+
 function createEntry(emojis) {
   return (
     <Entry
