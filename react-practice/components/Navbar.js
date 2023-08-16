f01353d92cc047d61842e14dc47eb7d3cc20cb03
Jessie He <104229166+jessieh9@users.noreply.github.com>
2023-08-15 20:41:07 -0700

    practicing styling in react

---
+++ react-practice/components/Navbar.js
@@ -0,0 +1,18 @@
+/**
+Challenge: Complete the Navbar to match the design
+
+Hint: use the Figma file for the most accurate peek at the design
+(colors, sizes, fonts, etc.)
+*/
+
+import React from "react"
+
+export default function Navbar() {
+    return (
+        <div className = "navigation">
+            <img src = "../images/react-icon-small.png" alt = "logo"/>
+            <h2>ReactFacts</h2>
+            <h3>React Course - Project 1</h3>
+        </div>
+    )
+}
